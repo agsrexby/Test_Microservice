241cb4eb5089ef9afa6f836a3b1759b4990d1baa
agsrexby <145462310+agsrexby@users.noreply.github.com>
2025-03-01 13:11:27 +0700

    Update SubscriptionDTO.java

--- src/main/java/com/example/userservice/dto/SubscriptionDTO.java
+++ src/main/java/com/example/userservice/dto/SubscriptionDTO.java
@@ -2,39 +2,15 @@ package com.example.userservice.dto;
 
 import lombok.Data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubscriptionDTO {
     private Long userId; // ID пользователя
     private String userName; // Имя пользователя
     private String serviceName; // Название сервиса
-
-    public SubscriptionDTO(Long userId, String userName, String serviceName) {
-        this.userId = userId;
-        this.userName = userName;
-        this.serviceName = serviceName;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getServiceName() {
-        return serviceName;
-    }
-
-    public void setServiceName(String serviceName) {
-        this.serviceName = serviceName;
-    }
 }
