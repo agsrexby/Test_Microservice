3164cbfba1afa10f4de39790b8734ff23014c3c9
agsrexby <145462310+agsrexby@users.noreply.github.com>
2025-03-01 13:12:50 +0700

    Update User.java

--- src/main/java/com/example/userservice/model/User.java
+++ src/main/java/com/example/userservice/model/User.java
@@ -19,41 +19,4 @@ public class User {
     @JsonIgnore
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Subscription> subscriptions;
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<Subscription> getSubscriptions() {
-        return subscriptions;
-    }
-
-    public void setSubscriptions(Set<Subscription> subscriptions) {
-        this.subscriptions = subscriptions;
-    }
-    public User(Long id) {
-        this.id = id;
-    }
-    public User(){
-
-    }
 }
